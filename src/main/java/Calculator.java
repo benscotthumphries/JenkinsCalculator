052c369104d9921ffa1c5b44a9536b61dc982ade
benscotthumphries <benscotthumphries@gmail.com>
2022-02-18 21:31:19 -0700

    update

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -39,7 +39,9 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        return 0;
+        if (n <= 1)
+            return n;
+        return fibonacciNumberFinder(n-1) + fibonacciNumberFinder(n-2);
     }
 
 
@@ -51,7 +53,7 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int n){
-        return null;
+        return Integer.toBinaryString(n);
     }
 
     /*
